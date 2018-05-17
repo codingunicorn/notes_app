157e7fe015793129001ba7f329ece50b17528504
Cécile Hefti <zena.nati@hotmail.ch>
2018-05-17 13:28:48 +0200

    feat: Add modal popup functionality

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
 "use strict";
 
 // Event Listeners for UI Elements
-let addNote = document.getElementsByClassName("add-note")[0];
-let modal = document.getElementsByClassName("modal")[0];
+const addNote = document.getElementsByClassName("add-note")[0];
+const cancelNote = document.getElementById("cancel-note");
 
-
-
-addNote.addEventListener('click', toggleModal(modal));
 
 // change Theme
 
@@ -17,7 +14,22 @@ function setTheme() {
     CSStheme.href = `./css/${chosenTheme}-theme.css`;
 }
 
-/* function toggleModal(modal) {
+/* function toggleModal() {
+    let modal = document.getElementsByClassName("modal")[0];
     modal.classList.toggle("invisible");
-}
- */
+    console.log("I was toggled");
+} */
+
+addNote.addEventListener('click', toggleModal());
+cancelNote.addEventListener('click', toggleModal());
+
+
+
+function toggleModal() {
+    let modal = document.getElementsByClassName("modal")[0];
+  
+    return function() {
+        modal.classList.toggle("invisible");
+        console.log("I was toggled");
+    };
+  }
