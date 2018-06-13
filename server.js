ff910491f5b72c8849c73f14f4f7228b4763c3c7
Cécile Hefti <zena.nati@hotmail.ch>
2018-06-13 15:57:15 +0200

    refactor: make Server work

--- server.js
+++ server.js
@@ -1,21 +1,41 @@
 var express = require('express');
 var http = require('http').Server(app);
-var app = express();
 var bodyparser = require('body-parser');
-var notesDB = require('./js/database.js');
+var path = require('path');
+//var notesDB = require('./js/database.js');
 
-//app.use(bodyparser.json());
-//app.use(bodyparser.urlencoded({extended: true})); 
+
+var app = express();
+
+
 app.use(express.static('js'));
 app.use(express.static('notes-pwa'));
 app.use('/static', express.static('public'));
 
+
+
+//Bodyparser Middleware
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: false}));
+
+
+//set static paths
+app.use(express.static(path.join(__dirname, 'public'))); //would have to create a public folder for that
 
 // Dateisystem
 app.get('/', express.static(__dirname));
 app.get('/js/', express.static(__dirname));
 app.route('/:filename').get((req, res) => { res.sendFile(__dirname + '/' + req.params.filename); });
 //app.route('/handlebars/:filename').get((req, res) => { res.sendFile(__dirname + '/handlebars/' + req.params.filename); });
+
+
+// vo dem YouTube tutorial
+app.get('/', function (req, res) {
+  res.render('index');
+  res.send('Server started');
+});
+
+
 
 // REST API
 /* app.route('/api/notes')
@@ -36,9 +56,6 @@ app.listen(2000, 'localhost', () => {
 /* var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
