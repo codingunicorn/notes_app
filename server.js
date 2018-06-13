45626fa31800e62e33a078937841b4221d744936
Cécile Hefti <zena.nati@hotmail.ch>
2018-06-13 19:18:04 +0200

    refactor: Refactor server

--- server.js
+++ server.js
@@ -8,28 +8,19 @@ var path = require('path');
 var app = express();
 
 
-app.use(express.static('js'));
-app.use(express.static('notes-pwa'));
-app.use('/static', express.static('public'));
-
-
-
 //Bodyparser Middleware
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: false}));
 
 
 //set static paths
-app.use(express.static(path.join(__dirname, 'public'))); //would have to create a public folder for that
+app.use(express.static(path.join(__dirname, 'public'))); //this makes my server work yay :)
 
-// Dateisystem
-app.get('/', express.static(__dirname));
-app.get('/js/', express.static(__dirname));
-app.route('/:filename').get((req, res) => { res.sendFile(__dirname + '/' + req.params.filename); });
+// Dateisystem für Handlebars (falls es das noch braucht)
 //app.route('/handlebars/:filename').get((req, res) => { res.sendFile(__dirname + '/handlebars/' + req.params.filename); });
 
 
-// vo dem YouTube tutorial
+// start Server(?)
 app.get('/', function (req, res) {
   res.render('index');
   res.send('Server started');
@@ -37,15 +28,9 @@ app.get('/', function (req, res) {
 
 
 
-// REST API
-/* app.route('/api/notes')
-.get((req, res) => {notesDB.getAllNotes(req, res);})
-.post(notesDB.addNewNote); */  
+// TODO: REST API
 
-/* app.route('/api/notes/:noteID')
-.get(notesDB.getOneNote)
-.delete(notesDB.removeOneNote)
-.put(notesDB.updateOneNote); */
+
 
 app.listen(2000, 'localhost', () => {
     console.log('server läuft'); 
@@ -53,10 +38,3 @@ app.listen(2000, 'localhost', () => {
 
 
 
-/* var express = require('express');
-var app = express();
-
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-}); */
